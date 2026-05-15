769a15d7033436c9b1e617b315e53a88f7335643
NSJP_kimura <m_kimura@nsjp.org>
2026-05-15 10:25:56 +0900

    AUTO_SEAL_50EVT

--- verify_chain.py
+++ verify_chain.py
@@ -1,8 +1,13 @@
-﻿import sys
-sys.path.insert(0, r"C:\Users\sirok\MoCKA")
-from schema.schema import verify_ledger
+import sys
+import os
 
+# スクリプト自身の場所からMoCKAルートを自動解決（Windows/Linux共通）
+ROOT = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, ROOT)
+
+from schema.schema import verify_ledger
 if verify_ledger():
     print("CHAIN OK")
 else:
     print("CHAIN ERROR")
+    sys.exit(1)
